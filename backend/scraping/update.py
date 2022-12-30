3658186090402be87238b752c470d764e9670a5f
tylerengland25 <tyler_england35@yahoo.com>
2022-12-29 17:19:52 -0800

    Rebound prop

--- backend/scraping/update.py
+++ backend/scraping/update.py
@@ -2,9 +2,11 @@ from update_shooting import main as scrape_shooting
 from update_scoring import main as scrape_scoring
 from update_game_details import main as scrape_game_details
 from odds import update_odds
+from schedule import update_schedule
 
 
 def update_all():
+    # update_schedule(2022)
     scrape_shooting()
     scrape_scoring()
     scrape_game_details()
