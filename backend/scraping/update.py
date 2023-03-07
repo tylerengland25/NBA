e414ef0f35cf47907bd8ce2c89af4919b258c1f2
Tyler England <tyler_england35@yahoo.com>
2023-03-06 22:45:47 -0800

    Up to date scrapers

--- backend/scraping/update.py
+++ backend/scraping/update.py
@@ -2,17 +2,15 @@ from update_shooting import update as update_scrape_shooting
 from update_scoring import update as update_scrape_scoring
 from update_game_details import update as update_game_details
 from update_game_totals import update as update_game_totals
-from odds import update_odds
 from schedule import update_schedule
 
 
 def update_all():
-    # update_schedule(2022)
-    # update_scrape_shooting()
+    update_schedule(2022)
+    update_scrape_shooting()
     update_scrape_scoring()
-    # update_game_details()
-    # update_game_totals()
-    # update_odds()
+    update_game_details()
+    update_game_totals()
 
 
 if __name__ == '__main__':
